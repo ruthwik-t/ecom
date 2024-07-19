895e4532105f590e6f028a50d94177be23c7b850
ruthwik-t <ruthwik2525@gmail.com>
2024-07-20 04:38:42 +0530

    add logic for user handlers

--- services/user/routes.go
+++ services/user/routes.go
@@ -1,6 +1,16 @@
 package user
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/go-playground/validator"
+	"github.com/ruthwik-t/ecom/services/auth"
+	"github.com/ruthwik-t/ecom/types"
+	"github.com/ruthwik-t/ecom/utils"
+)
 
 type Handler struct {
 	store *Store
@@ -13,12 +23,104 @@ func NewHandler(store *Store) *Handler {
 func (h *Handler) RegisterRoutes(router *http.ServeMux) {
 	router.HandleFunc("GET /login", h.handleLogin)
 	router.HandleFunc("GET /register", h.handleRegister)
+
+	router.HandleFunc("GET /users/{userID}", h.handleGetUser)
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to LOGIN route"))
+	var user types.LoginUserPayload
+	if err := utils.ParseJSON(r, &user); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(user); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
+
+	u, err := h.store.GetUserByEmail(user.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		return
+	}
+
+	if !auth.ComparePasswords(u.Password, []byte(user.Password)) {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid email or password"))
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, map[string]string{"token": ""})
+
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to REGISTER route"))
+	var user types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &user); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(user); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	_, err := h.store.GetUserByEmail(user.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", user.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(user.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.store.CreateUser(types.User{ //error here
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+}
+
+func (h *Handler) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	userID, err := extractUserIDFromPath(r.URL.Path)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	user, err := h.store.GetUserByID(userID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, user)
+
+}
+
+func extractUserIDFromPath(path string) (int, error) {
+	segments := strings.Split(path, "/")
+	if len(segments) < 3 || segments[1] != "users" {
+		return 0, fmt.Errorf("missing user ID")
+	}
+
+	userID, err := strconv.Atoi(segments[2])
+	if err != nil {
+		return 0, fmt.Errorf("invalid user ID")
+	}
+
+	return userID, nil
 }
--- services/user/store.go
+++ services/user/store.go
@@ -1,6 +1,10 @@
 package user
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/ruthwik-t/ecom/types"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +12,16 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	return nil, nil
+}
+
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (s *Store) CreateUser(user types.User) error {
+	return nil
 }
