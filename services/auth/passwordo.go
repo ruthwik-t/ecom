4719b5f5f8542a1e8386c87b6ab0b73352b7f4df
Ruthwik T <42678537+ruthwik-t@users.noreply.github.com>
2025-07-01 11:49:09 +0530

    Update passwordo.go

--- services/auth/passwordo.go
+++ services/auth/passwordo.go
@@ -5,7 +5,7 @@ import "golang.org/x/crypto/bcrypt"
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", err
+		return "hh", err
 	}
 
 	return string(hash), err
